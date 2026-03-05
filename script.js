0e95f06cd47a5351f2fc5948c07a2165d9ccd099
Yaemiko-nakurami-shrine <damien.brito@escola.pr.gov.br>
2026-03-05 11:49:45 +0000

    a

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 <script>
     document.getElementById('wangsheng-form').addEventListener('submit', function(e) {
-        e.preventDefault();
-        alert('Aiya! Pedido recebido. A Diretora Hu Tao entrará em contato em breve... ou te visitará à noite! 👻');
-    });
+        e.preventDefault()
+    }alert('Aiya! Pedido recebido. A Diretora Hu Tao entrará em contato em breve... ou te visitará à noite! 👻');
 </script>
