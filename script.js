be9faf4b29c4b42a2124ed9f642eb9411f97e181
Yaemiko-nakurami-shrine <damien.brito@escola.pr.gov.br>
2026-03-05 11:55:31 +0000

    a

--- script.js
+++ script.js
@@ -3,3 +3,15 @@
         e.preventDefault()
     }alert('Aiya! Pedido recebido. A Diretora Hu Tao entrará em contato em breve... ou te visitará à noite! 👻');
 </script>
+const coupons = [
+    "COMPRE 1, LEVE 2! (Para você e um amigo)",
+    "DESCONTO DE 77% - Válido apenas para fantasmas",
+    "CUPOM EXPIROU... igual ao cliente! Hehe~",
+    "VALE-LANCHE: Um lanche grátis no Quiosque da Wanmin",
+    "CLIENTE VIP: Caixão com estofado de seda de Liyue"
+];
+
+document.getElementById('get-coupon').addEventListener('click', () => {
+    const random = coupons[Math.floor(Math.random() * coupons.length)];
+    document.getElementById('coupon-text').innerText = random;
+});
