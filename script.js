e44a681a375fad44ba1a9db39e19a9dce14b6407
Yaemiko-nakurami-shrine <damien.brito@escola.pr.gov.br>
2026-03-05 11:41:56 +0000

    fuck.

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+    document.getElementById('wangsheng-form').addEventListener('submit', function(e) {
+        e.preventDefault();
+        alert('Aiya! Pedido recebido. A Diretora Hu Tao entrará em contato em breve... ou te visitará à noite! 👻');
+    });
+</script>
